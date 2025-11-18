664c1a9120f41507f1d4ebfc6ff1b065f9b7f34d
Lenovo_yeison <yelondonoo@unal.edu.co>
2025-11-17 22:01:16 -0500

    Finalizacion Dockerizar los Microservicios

--- Microservicio_Recomendaciones/app.py
+++ Microservicio_Recomendaciones/app.py
@@ -1,12 +1,19 @@
 from flask import Flask, jsonify, request
 from pymongo import MongoClient
+import os
 
 app = Flask(__name__)
-client= MongoClient("mongodb://localhost:27017/")
-db = client["microservicio_recomendaciones"]
+
+MONGO_HOST = os.getenv("MONGO_HOST", "mongo_recomendaciones")
+MONGO_PORT = int(os.getenv("MONGO_PORT", 27017))
+MONGO_DB = os.getenv("MONGO_DB", "microservicio_recomendaciones")
+
+
+client = MongoClient(f"mongodb://{MONGO_HOST}:{MONGO_PORT}/")
+db = client[MONGO_DB]
 recomendaciones_collection = db["recomendaciones"]
 
-@app.route('/agregar_recomendacion', methods = ['POST'])
+@app.route('/agregar_recomendacion', methods=['POST'])
 def crear_recomendacion():
     data = request.get_json()
     result = recomendaciones_collection.insert_one({
@@ -15,35 +22,35 @@ def crear_recomendacion():
         "categoria": data["categoria"],
         "recomendacion": data["recomendacion"]
     })
-    if result.inserted_id is None:
-        return jsonify({"message": "No se pudo agregar la recomendacion"}), 500
+    if result.inserted_id:
+        return jsonify({"message": "Recomendación agregada exitosamente"}), 201
     else:
-        return jsonify({"message": "Recomendacion agregada exitosamente"}), 201
+        return jsonify({"message": "No se pudo agregar la recomendación"}), 500
 
-@app.route('/recomendar', methods = ['POST'])
+@app.route('/recomendar', methods=['POST'])
 def recomendar():
     data = request.get_json()
     peso = float(data["peso"])
     altura = float(data["altura"])
 
-    if (altura <=0 or peso <=0):
-        return jsonify({"error": "Altura o Peso no valido"}), 400
-    
-    imc = peso/(altura * altura)
-    
+    if altura <= 0 or peso <= 0:
+        return jsonify({"error": "Altura o peso no válido"}), 400
+
+    imc = peso / (altura * altura)
+
     regla = recomendaciones_collection.find_one({
         "imc_min": {"$lte": imc},
         "imc_max": {"$gte": imc}
     })
 
     if not regla:
-        return jsonify({"message": "No existe una recomendacion para este IMC"}), 404
-    
+        return jsonify({"message": "No existe una recomendación para este IMC"}), 404
+
     return jsonify({
-        "imc": round(imc,2),
+        "imc": round(imc, 2),
         "categoria": regla["categoria"],
         "recomendacion": regla["recomendacion"]
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host='0.0.0.0', port=5003, debug=True)
